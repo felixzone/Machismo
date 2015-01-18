22e2107cb53c21d6cc8d1b50e4a6260c3c904630
Felix Liu <felix@Felix-PC.lan>
2015-01-18 20:27:40 +0800

    Initial Commit

---
+++ Machismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Machismo
+//
+//  Created by Felix Liu on 18/1/15.
+//  Copyright (c) 2015 Felix Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Machismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Machismo
+//
+//  Created by Felix Liu on 18/1/15.
+//  Copyright (c) 2015 Felix Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
